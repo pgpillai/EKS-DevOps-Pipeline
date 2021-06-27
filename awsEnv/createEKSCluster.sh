f1399d42269c25b1ce80f5e123d5b157d9b76a54
pgpillai <pgpillai@yahoo.com>
2021-06-27 15:13:08 -0400

    added Cluster creation yml

--- awsEnv/createEKSCluster.sh
+++ awsEnv/createEKSCluster.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-role=$(aws iam get-role --role-name DevopsEKSClusterRole --query "Role.RoleName")
-echo "Role name is $role"
-if [[ -z $role || $role != 'EKSDevOpsClusterRole' ]]
-then
-aws iam create-role \
-  --role-name DevopsEKSClusterRole \
-  --assume-role-policy-document file://"cluster-role-trust-policy.json"
+
+# role=$(aws iam get-role --role-name DevopsEKSClusterRole --query "Role.RoleName")
+# echo "Role name is $role"
+# if [[ -z $role || $role != 'EKSDevOpsClusterRole' ]]
+# then
+# aws iam create-role \
+#   --role-name DevopsEKSClusterRole \
+#   --assume-role-policy-document file://"cluster-role-trust-policy.json"
 
 
-aws iam attach-role-policy \
-  --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy \
-  --role-name DevopsEKSClusterRole
-fi
+# aws iam attach-role-policy \
+#   --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy \
+#   --role-name DevopsEKSClusterRole
+# fi
 
-delete=true;
-if $delete == true
-then
-aws iam detach-role-policy \
-   --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy \
-   --role-name DevopsEKSClusterRole
-aws iam delete-role --role-name DevopsEKSClusterRole
-fi
+# delete=true;
+# if $delete == true
+# then
+# aws iam detach-role-policy \
+#    --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy \
+#    --role-name DevopsEKSClusterRole
+# aws iam delete-role --role-name DevopsEKSClusterRole
+# fi
